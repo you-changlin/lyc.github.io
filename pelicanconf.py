70822aef60e970f7919deb90a777911cbc008f47
lyc <lyc0931696618@gmail.com>
2021-10-16 23:54:57 +0800

    test

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = 'CMSiMDE 網際內容管理'
+AUTHOR = 'lyc'
+SITENAME = 'lyc blog'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
